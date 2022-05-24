419a4296b3560c17ade272480c87acf7344c51b3
bencevicbruno <bruno.bencevic@cobeisfresh.com>
2022-05-24 21:02:39 +0200

    Initial Commit

---
+++ SwimPal/SwimPalApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwimPalApp.swift
+//  SwimPal
+//
+//  Created by Bruno Benčević on 24.05.2022..
+//
+
+import SwiftUI
+
+@main
+struct SwimPalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
